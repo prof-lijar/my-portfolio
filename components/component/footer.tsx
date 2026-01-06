d88cfcea915b314c5a862dc4914bae916fb911c4
LI JAR <davidbeljar@gmail.com>
2026-01-07 00:38:39 +0900

    feat: extra curriculur activities (#5)

--- components/component/footer.tsx
+++ components/component/footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
           </Avatar>
           <span className="font-semibold">LI JAR | Portfolio</span>
         </div>
-        <p>© 2025 LI JAR. All rights reserved.</p>
+        <p>© 2026 LI JAR. All rights reserved.</p>
       </div>
     </footer>
   );
