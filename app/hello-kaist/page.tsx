b766f5dda58b8f7d716c0101bd682d125dad4505
LI JAR <davidbeljar@gmail.com>
2026-01-07 01:07:37 +0900

    feat: add research publication (#6)

--- app/hello-kaist/page.tsx
+++ app/hello-kaist/page.tsx
@@ -1,11 +1,13 @@
 import KaistShowcase from '@/components/hello-kaist/kaist-showcase';
 import ExtracurricularActivities from '@/components/hello-kaist/extracurricular-activities';
+import ResearchPapers from '@/components/hello-kaist/research-papers';
 
 export default function HelloKaistPage() {
   return (
     <>
       <ExtracurricularActivities />
       <KaistShowcase />
+      <ResearchPapers />
     </>
   );
 }
